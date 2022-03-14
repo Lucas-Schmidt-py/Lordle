8da17294b226abffab59df308698b72adc7536f4
Lucas-Schmidt-py <71510679+Lucas-Schmidt-py@users.noreply.github.com>
2022-03-14 17:12:06 +0100

    Del-Button

--- Beat-Layout/code.js
+++ Beat-Layout/code.js
@@ -1,7 +1,16 @@
 function firstWord (letter) {
-    document.getElementById('Word1').innerHTML += letter
+    document.getElementById('Word1').innerHTML += letter;
 }
 
 function WordInput (letter){
-    firstWord(letter)
+    firstWord(letter);
+}
+
+function WordDes(){
+    firstDel();
+}
+
+function firstDel(){
+    let str = (document.getElementById('Word1').innerHTML).slice(0, -1);
+    document.getElementById('Word1').innerHTML = str
 }
