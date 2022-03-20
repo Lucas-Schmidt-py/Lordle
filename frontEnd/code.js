2ac470bbd65741e1483ea3c61cd10988fb7e0a57
Lucas-Schmidt-py <71510679+Lucas-Schmidt-py@users.noreply.github.com>
2022-03-20 22:18:18 +0100

    Help Button (Incomplete)

--- frontEnd/code.js
+++ frontEnd/code.js
@@ -16,3 +16,11 @@ function WordDel(souce){
     let str = (document.getElementById(souce).innerHTML).slice(0, -1);
     document.getElementById('Word1').innerHTML = str
 }
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  } 
