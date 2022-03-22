7fb8e6a95adc0cb7dde2ac7a7d5ccfb90b7e2922
Lucas-Schmidt-py <71510679+Lucas-Schmidt-py@users.noreply.github.com>
2022-03-22 13:50:23 +0100

    physical-keyboard-input

---
+++ frontEnd/keyboard.js
@@ -0,0 +1,11 @@
+document.addEventListener('keydown', function(event){
+    if(event.keyCode > 64 && event.keyCode < 91){
+        wordInput(event.key.toUpperCase());
+    }else if(event.keyCode===46 || event.keyCode===8){
+        wordDes();
+    }else if(event.keyCode===13){
+        wordInput('ENTER');
+    }else if(event.keyCode===32){
+        wordInput('SPACE');
+    }
+});
