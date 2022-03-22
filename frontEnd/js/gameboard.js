afa0cbf3e6ecc88a1911ce039ae665b29cb91d84
minhtomato <bmf.phan@gmail.com>
2022-03-22 17:22:22 +0100

    compareWords

--- frontEnd/js/gameboard.js
+++ frontEnd/js/gameboard.js
@@ -1,8 +1,8 @@
 let row = 0;
 let collum = 0;
 let current_word= "";
-let word_lenght = 5;
-let row_lenght = 6;
+let WORD_LENGTH = 5;
+let ROW_LENGTH = 6;
 
 function wordInput (letter){
     if(letter==='ENTER'){      
@@ -12,18 +12,18 @@ function wordInput (letter){
             row += 1;
             current_word= "";        
 
-            if(row===row_lenght){
+            if(row===ROW_LENGTH){
                 //result screen
             }
         //}
     }else{
-        if(collum < word_lenght && row < row_lenght){
+        if(collum < WORD_LENGTH && row < ROW_LENGTH){
             if(letter==='SPACE'){
                 letter='/'
             }
             current_word += letter;
             let all_tiles = document.querySelectorAll('game-tile');
-            all_tiles[row*word_lenght+collum].innerHTML = letter;
+            all_tiles[row*WORD_LENGTH+collum].innerHTML = letter;
             collum += 1;
         }
     }
@@ -36,8 +36,48 @@ function wordDes(){
         collum -= 1;
         current_word = current_word.slice(0, -1);
         let all_tiles = document.querySelectorAll('game-tile');
-        all_tiles[row*word_lenght+collum].innerHTML = " ";        
+        all_tiles[row*WORD_LENGTH+collum].innerHTML = " ";        
     }
 
     document.getElementById('test').innerHTML = current_word;
+}
+
+//removes word[index] from word
+function removeChar(word, index){
+    return word.slice(0,index) + word.slice(index + 1);
+}
+
+
+//input: 2 strings 
+/**description: for each position in ROW_LENGTH concatenate a str corresponding whether the letter 
+is (1)in the same position, (2) included, (3) or not included in str2**/
+function compareWords(str1, str2){
+    let result = '';
+    let letters_not_found = str2;
+
+    for(let i = 0; i < WORD_LENGTH; i++){
+        if(i >= str1.length){
+            result += '3';
+            continue;
+        }
+
+        index = letters_not_found.indexOf(str1[i]);
+        
+        if(i < str2.length){
+            if(str1[i] === str2[i]){
+                result += '1';
+                letters_not_found = removeChar(letters_not_found, index);
+                continue;
+            }
+        }
+
+        if(index !== -1){
+            result += '2';
+            letters_not_found = removeChar(letters_not_found, index);
+        }
+        else{
+            result += '3';
+        }
+    }
+    return result;
 }
